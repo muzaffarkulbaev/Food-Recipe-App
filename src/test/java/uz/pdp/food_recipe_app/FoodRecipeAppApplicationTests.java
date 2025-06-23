944ad5ed3296132e3c2d0c62b8cbb438d58a239f
muzaffarkulbaev <muzaffarkulbaev95@gmail.com>
2025-06-23 14:59:17 +0500

    first commit

---
+++ src/test/java/uz/pdp/food_recipe_app/FoodRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.food_recipe_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
