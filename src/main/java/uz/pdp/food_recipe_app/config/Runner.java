f294375092aff80fb1fb498f4408faff475a2341
muzaffarkulbaev <muzaffarkulbaev95@gmail.com>
2025-06-23 15:18:15 +0500

    Runner class is existing

---
+++ src/main/java/uz/pdp/food_recipe_app/config/Runner.java
@@ -0,0 +1,10 @@
+package uz.pdp.food_recipe_app.config;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Runner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
